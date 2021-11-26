f95d716e88a91c4fe69826532beae978a7981e03
thekeyhi <2thekey@naver.com>
2021-11-26 17:12:17 +0900

    20211126_로또디비읽기 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,67 @@
-import 'dart:async';
-
-import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-var num=List.generate(1000,(i) => List(10), growable:false);
-
+var dd="";
 void main() async {
-  // int lotto_cha=1;
-  // String url= "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$lotto_cha";
-  // var response = await http.get(url);
-  //
-  // Map<String, dynamic> lotto = json.decode(response.body);
-  //
-  // num[lotto_cha][0] = lotto["drwNo"];
-  // num[lotto_cha][1] = lotto["drwNoDate"];
-  // num[lotto_cha][2] = lotto["drwtNo1"];
-  // num[lotto_cha][3] = lotto["drwtNo2"];
-  // num[lotto_cha][4] = lotto["drwtNo3"];
-  // num[lotto_cha][5] = lotto["drwtNo4"];
-  // num[lotto_cha][6] = lotto["drwtNo5"];
-  // num[lotto_cha][7] = lotto["drwtNo6"];
-  // num[lotto_cha][8] = lotto["bnusNo"];
-  // num[lotto_cha][9] = lotto["returnValue"];
-  // lotto_cha++;
-  //
-  //
-  // while(num[lotto_cha-1][9]=="success") {
-  //   url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$lotto_cha";
-  //   response = await http.get(url);
-  //   lotto = json.decode(response.body);
-  //   num[lotto_cha][0] = lotto["drwNo"];
-  //   num[lotto_cha][1] = lotto["drwNoDate"];
-  //   num[lotto_cha][2] = lotto["drwtNo1"];
-  //   num[lotto_cha][3] = lotto["drwtNo2"];
-  //   num[lotto_cha][4] = lotto["drwtNo3"];
-  //   num[lotto_cha][5] = lotto["drwtNo4"];
-  //   num[lotto_cha][6] = lotto["drwtNo5"];
-  //   num[lotto_cha][7] = lotto["drwtNo6"];
-  //   num[lotto_cha][8] = lotto["bnusNo"];
-  //   num[lotto_cha][9] = lotto["returnValue"];
-  //
-  //   lotto_cha++;
-  // } //while
-  //
-  //
-  // print('aa');
-  //  print(num[980]);
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-class _MyAppState extends State<MyApp> {
-  //FirebaseFirestore fireStore = FirebaseFirestore.instance;
+class MyApp extends StatelessWidget {
+  var fsconnect = FirebaseFirestore.instance;
+
+
+  myget() async {
+    await fsconnect.collection('books')
+        .doc('lotto')
+        .get()
+        .then((DocumentSnapshot ds) {
+      // ignore: unnecessary_statements
+      dd=ds.data()['bunho'];
+      //print(dd);
+    }
+    );
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference fireStore = FirebaseFirestore.instance.collection('books');
-    return FutureBuilder<DocumentSnapshot>(
-        future: fireStore.doc('lotto').get(),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          return MaterialApp(
-            home: Scaffold(
-              appBar: AppBar(
-                title: Text("hello world"),
+
+    myget();
+
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Firebase Firestore App'),
+          ),
+          body: Column(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('send data'),
+                onPressed: () {
+                  fsconnect.collection("books").add({
+                    'name': 'sarah',
+                    'title': 'xyz',
+                    'email': 'sarah@gmail.com',
+                  });
+                  print("send ..");
+                },
               ),
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  ElevatedButton(
-                    //color: Colors.blue,
-                    child: Text("create button",style: TextStyle(color : Colors.white)),
-                    onPressed: (){
-                      //클릭시 데이터를 추가해준다.
-                    },
-                  ),
-                  ElevatedButton(
-                    //color: Colors.blue,
-                    child: Text("read button", style: TextStyle(color : Colors.white)),
-                    onPressed: (){
+              RaisedButton(
+                child: Text('get data'),
+                onPressed: () {
 
-                      Map<String, dynamic> data = snapshot.data.data() as Map<String, dynamic>;
-                      print(data['bunho'] );
+                  print(dd);
+                },
+              )
 
+            ],
+          ),
 
-                    },
-                  ),
-                  ElevatedButton(
-                    //color: Colors.blue,
-                    child: Text("update button", style: TextStyle(color : Colors.white)),
-                    onPressed: (){
-                      //클릭시 데이터를 갱신해준다.
-                    },
-                  ),
-                  ElevatedButton(
-                    //color: Colors.blue,
-                    child: Text("delete button", style: TextStyle(color : Colors.white)),
-                    onPressed: (){
-                      //클릭시 데이터를 삭제해 준다.
-                    },
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
-    );
+        ));
   }
 }
