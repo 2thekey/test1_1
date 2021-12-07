095571964d70ba7e7d5e387e8230775405af3cba
thekeyhi <2thekey@naver.com>
2021-12-07 20:42:48 +0900

    20211207_do it 디비리드 후 2차원배열에 입력 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-var dd="";
+var num=List.generate(1000,(i) => List(10), growable:false);
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -13,15 +15,40 @@ void main() async {
 class MyApp extends StatelessWidget {
   var fsconnect = FirebaseFirestore.instance;
 
-
   myget() async {
+    int aa=0;
+    String bb;
+    await fsconnect.collection('naepo_lotto').orderBy("drwNo", descending: false).snapshots().listen((data) {
+    data.docs.forEach((d) {
 
-    var data = await fsconnect.collection('naepo_lotto').get();
-    var details = data.docs.toList();
+      bb=(d.id).toString();
+      //print(aa);
+      num[aa+1][0]=(data.docs[aa]['bnusNo']);
+      num[aa+1][1]=(data.docs[aa]['drwtNo1']);
+      num[aa+1][2]=(data.docs[aa]['drwtNo2']);
+      num[aa+1][3]=(data.docs[aa]['drwtNo3']);
+      num[aa+1][4]=(data.docs[aa]['drwtNo4']);
+      num[aa+1][5]=(data.docs[aa]['drwtNo5']);
+      num[aa+1][6]=(data.docs[aa]['drwtNo6']);
+      num[aa+1][7]=(data.docs[aa]['bnusNo']);
+      num[aa+1][8]=(data.docs[aa]['drwNoDate']);
+     // print(data.docs[aa]['bnusNo']);
+      aa++;
 
-    details.forEach((d) {
-      print(d.drwNo);
     });
+
+    });
+
+
+    // var soon='2';
+    // await fsconnect.collection('naepo_lotto')
+    //     .doc(soon)
+    //     .get()
+    //     .then((DocumentSnapshot ds) {
+    //   // ignore: unnecessary_statements
+    //   dd=ds.data()['bnusNo'].toString();
+    //  // print(dd);
+    // });
 
     // await fsconnect.collection('books')
     //     .doc('lotto')
@@ -30,11 +57,12 @@ class MyApp extends StatelessWidget {
     //   // ignore: unnecessary_statements
     //   dd=ds.data()['bunho'];
     //   //print(dd);
-    }
+    // }
+    // );
+
+        }
 
 
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,7 +91,11 @@ class MyApp extends StatelessWidget {
                 child: Text('get data'),
                 onPressed: () {
 
-                  print(dd);
+                  print(num[1][1]);
+                  print(num[100][1]);
+                  print(num[101][1]);
+                  print(num[102][1]);
+
                 },
               )
 
